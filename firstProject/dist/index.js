fe8c2f64dc39595f4627645c51d82235670067cd
DejFcoder <trebatickydavid670@gmail.com>
2024-06-15 08:33:25 +0200

    update

--- firstProject/dist/index.js
+++ firstProject/dist/index.js
@@ -1,10 +1,46 @@
 "use strict";
+/* class User {
+  public email: string;
+  private name: string;
+  readonly city: string = "London";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+} */
 class User {
     constructor(email, name) {
-        this.city = "London";
         this.email = email;
         this.name = name;
+        this._courseCount = 1;
+        this.city = "London";
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count must be greater than 1");
+        }
+        this.courseCount = courseNum;
     }
 }
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+    changeCourseCount() {
+        this._courseCount = 2;
+    }
+}
+;
 const david = new User("david@gmail.com", "david");
-/* david.city = ""; */
+//david.name
+//david.deleteToken()
--- firstProject/src/index.ts
+++ firstProject/src/index.ts
@@ -1,13 +1,50 @@
-class User {
-  email: string;
-  name: string;
+/* class User {
+  public email: string;
+  private name: string;
   readonly city: string = "London";
-
   constructor(email: string, name: string) {
     this.email = email;
     this.name = name;
   }
+} */
+
+class User {
+  protected _courseCount = 1;
+
+  readonly city: string = "London";
+  constructor(
+    public email: string,
+    public name: string
+  ) //private userId: string
+  {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple ${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error("Course count must be greater than 1");
+    }
+    this.courseCount = courseNum;
+  }
 }
 
+class SubUser extends User {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 2;
+  }
+};
+
 const david = new User("david@gmail.com", "david");
-/* david.city = ""; */
+//david.name
+//david.deleteToken()
